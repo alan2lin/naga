b4d033405c01817e16d24f2348d25b91832116fc
alan2lin <workspace.public@gmail.com>
2023-11-24 04:30:45 +0800

    临时保存

---
+++ start/src/test/java/com/alan2lin/bbs/test/SuiteTests.java
@@ -0,0 +1,18 @@
+package com.alan2lin.bbs.test;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Suite
+@SuiteDisplayName("测试套件，分组测试")
+@SelectPackages("com.alan2lin.bbs.test")
+public class SuiteTests {
+
+    public static void setup(){
+        System.out.println("BeforeAll init() method called========================================");
+    }
+}
