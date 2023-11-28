466db4d33f4785a9d9d4afb90d9b3bcf8ca3ce72
alan2lin <abc@example.com>
2023-11-29 02:17:48 +0800

    采用 shardingshpere 5.4.2 snapshot 版本， 在本地修改了源代码而升级 ，可以 到 https://github.com/alan2lin/shardingsphere 下载

--- start/src/main/resources/db/migration/V2__init_test.sql
+++ start/src/main/resources/db/migration/V2__init_test.sql
@@ -1,9 +1,9 @@
 INSERT INTO "public"."test" ("id", "name", "value") VALUES (1, 'alan', 'hello');
 INSERT INTO "public"."test" ("id", "name", "value") VALUES (2, 'bob', 'world');
 
-INSERT INTO "public"."tshard_0" ("id", "name", "value") VALUES (1, 'bob1', 'world1');
-INSERT INTO "public"."tshard_0" ("id", "name", "value") VALUES (3, 'bob3', 'world3');
-INSERT INTO "public"."tshard_0" ("id", "name", "value") VALUES (5, 'bob4', 'world4');
-INSERT INTO "public"."tshard_1" ("id", "name", "value") VALUES (2, 'bob2', 'world2');
-INSERT INTO "public"."tshard_1" ("id", "name", "value") VALUES (4, 'bob4', 'world4');
-INSERT INTO "public"."tshard_1" ("id", "name", "value") VALUES (6, 'bob6', 'world6');
+INSERT INTO "public"."tshard_1" ("id", "name", "value") VALUES (1, 'bob1', 'world1');
+INSERT INTO "public"."tshard_1" ("id", "name", "value") VALUES (3, 'bob3', 'world3');
+INSERT INTO "public"."tshard_1" ("id", "name", "value") VALUES (5, 'bob4', 'world4');
+INSERT INTO "public"."tshard_0" ("id", "name", "value") VALUES (2, 'bob2', 'world2');
+INSERT INTO "public"."tshard_0" ("id", "name", "value") VALUES (4, 'bob4', 'world4');
+INSERT INTO "public"."tshard_0" ("id", "name", "value") VALUES (6, 'bob6', 'world6');
